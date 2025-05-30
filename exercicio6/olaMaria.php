4cdeaad1cfe4657d953ae13863b121c329943c43
Bruno Choutock Yukimitu <brunochoutockyk@gmail.com>
2025-05-30 19:10:03 -0300

    a

---
+++ exercicio6/olaMaria.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá Maria</title>
+</head>
+<body>
+    <form name="OlaMaria" method="post" action="fala.php">
+        <button type="submit">Falar com alguem</button>
+    </form>
+</body>
+</html>
