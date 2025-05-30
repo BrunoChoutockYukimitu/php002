8c85034d16386e07ead99288d303549dd681e31c
Bruno Choutock Yukimitu <brunochoutockyk@gmail.com>
2025-05-30 19:09:02 -0300

    adas

--- exercicio003/tabuada8.php
+++
@@ -1,7 +0,0 @@
-<?php
-$tabuada == 0;
-while($tabuada <10){
-$tabuada = $tabuada + 1;
-echo  $tabuada * 8;
-}
-?>
--- exercicios/exercicio011/index.php
+++ exercicios/exercicio011/index.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Olá Maria</title>
+    <title>Idade</title>
 </head>
 <body>
-    <form name="OlaMaria" method="post" action="fala.php">
-        <button type="submit">Falar com alguem</button>
+    <form name="formulario" method="post" action="msg.php">
+        Nome: <input type="text" name="nome"><br>
+        <button type="submit">Aplicar</button>
     </form>
 </body>
 </html>
